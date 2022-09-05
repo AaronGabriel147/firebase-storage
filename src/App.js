974fed6e43dc8d0e3e014946e55fb34c652886b0
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-09-05 16:02:45 -0700

    dark mode complete

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ export default function App() {
 
   return (
     <div className="app">
-      <h1>React Template</h1>
-      <p>Lorem ipsum. This is a p tag</p>
+      <h1>CSS RESET</h1>
+      <h2>&</h2>
+
+      <p>Custom styling.</p>
       <div className="border"></div>
+      <div class="input-cont">
+        <label>Name</label>
+        <input type="input" placeholder="Name*" name="name" id='name' required />
+      </div>
       <button>Submit</button>
     </div >
   );
