84278c84b3bff364ca17e27749bc75b252135c88
Aaron Gabriel <aarongabriel147@gmail.com>
2022-09-09 10:00:15 -0700

    mobile friendly

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -2,11 +2,23 @@ import React from 'react';
 import useFirestore from '../hooks/useFirestore';
 import { motion } from 'framer-motion';
 
+// import { deleteDoc, doc } from "firebase/firestore";
+import { doc, deleteField } from "firebase/firestore";
+import { projectFirestore } from '../firebase';
+
+
+
 const ImageGrid = ({ setSelectedImg }) => {
     const { docs } = useFirestore('images');
 
+    console.log(docs[0].id);
+
+
+    // const removeQuote = id => deleteDoc(doc(projectFirestore, "images", id))
+
     return (
-        <div className="img-cont    ">
+        <div className="img-cont">
+            {console.log(docs)}
             {docs && docs.map(doc => (
                 <motion.div className="img-wrap" key={doc.id}
                     layout
@@ -18,9 +30,16 @@ const ImageGrid = ({ setSelectedImg }) => {
                         animate={{ opacity: 1 }}
                         transition={{ delay: 1 }}
                     />
+                    {/* <button onClick={() => removeQuote(doc.id)}>Remove</button> */}
+
+
+
                 </motion.div>
-            ))}
-        </div>
+
+
+            ))
+            }
+        </div >
     )
 }
 
