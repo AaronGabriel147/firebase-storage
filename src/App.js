3732697eb43517516227385243c9e3a7e8549abf
Aaron Gabriel <aarongabriel147@gmail.com>
2022-09-14 12:39:47 -0700

    template edited and improved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 
@@ -6,12 +7,28 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-
+  const [quotes, setQuotes] = useState([{
+    author: ''
+  }]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("submit.");
+    setUsername('')
   };
+
+
+  useEffect(() => {
+    axios
+      .get('https://thestoics.herokuapp.com/quotes/1170')
+      .then(res => {
+        setQuotes(res.data)
+        console.log('@@@', quotes)
+
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log('@@@', quotes).author
 
   return (
     <div className="app">
@@ -37,14 +54,56 @@ function App() {
             onChange={(e) => setPassword(e.target.value)}
           />
 
-
           <button>SUBMIT</button>
         </div>
       </form>
 
 
+      <div className="line-separator"></div>
 
-    </div>
+
+      <div>
+
+        {quotes.map((quote, index) => (
+          <div key={index}>
+            <h2>{quote.author}</h2>
+          </div>
+        ))}
+      </div>
+
+
+      {/* {quotes.sort((a, b) => a.id - b.id).reverse().map((quote) => (
+                    <div className='card-container' key={"card-container-" + quote.id}>
+                        <div className="card-text">
+                            <p>{quote.author}</p>
+                            <p>{quote.source}</p>
+                        </div>
+                        <p className='quote-text'>{quote.quote}</p>
+                        <div className="update-delete-buttons">
+
+
+                            <Delete
+                                quotes={quotes}
+                                setQuotes={setQuotes}
+                                id={quote.id}
+                            />
+
+                            <Update
+                                quote={quote}
+                                quotes={quotes}
+                                setQuotes={setQuotes}
+                                setFormData={setFormData}
+                            />
+
+                        </div>
+                    </div>
+                ))} */}
+
+
+
+
+
+    </div >
   );
 }
 
